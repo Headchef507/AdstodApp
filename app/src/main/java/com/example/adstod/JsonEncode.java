abb794dff0fdd33b91835f25c95a68c960843c98
Headchef507 <32708897+Headchef507@users.noreply.github.com>
2019-04-09 17:14:40 +0000

    Encode ready?
    Encoding ready

--- app/src/main/java/com/example/adstod/JsonEncode.java
+++ app/src/main/java/com/example/adstod/JsonEncode.java
@@ -1,5 +1,21 @@
 package com.example.adstod;
 
+import org.json.JSONObject;
+import org.json.JSONException;
+
 public class JsonEncode {
+    public JSONObject getJSONQuestions(String language) throws JSONException {
+        JSONObject obj = new JSONObject();
+
+        obj.put("name", language);/*
+        obj.put("num", new Integer(100));
+        obj.put("balance", new Double(1000.21));
+        obj.put("is_vip", new Boolean(true));
+        */
+
+        System.out.print(obj);
+        return obj;
+    }
+
 
 }
